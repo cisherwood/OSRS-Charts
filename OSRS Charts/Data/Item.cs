aa1921f1646e4f5109d283f3528f46f1113c53ea
ChrisPCA <cisherwood@p-ca.com>
2020-01-24 16:35:56 -0500

    Initial

---
+++ OSRS Charts/Data/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OSRS_Charts.Data
+{
+    public class Item
+    {
+        public int ItemID { get; set; }
+
+        [MaxLength(500)]
+        public string Name { get; set; }
+    }
+}
